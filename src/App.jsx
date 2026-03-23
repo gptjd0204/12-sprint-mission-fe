25093c11af3d8d223750ebfd48d48fa1ea0fd8a6
gptjd0204 <gptjd0204@naver.com>
2026-03-23 14:53:07 +0900

    feat: Footer 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import GNB from "./components/GNB";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <GNB />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import facebookIc from "../assets/ic_facebook.svg";
+import xIc from "../assets/ic_twitter.svg";
+import instagramIc from "../assets/ic_instagram.svg";
+import youtubeIc from "../assets/ic_youtube.svg";
+import styles from "../styles/Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className="regular">
+      <div className={styles.copylight}>
+        <p>©codeit - 2024</p>
+      </div>
+      <div className={styles.info}>
+        {/* 추후 Link로 수정 */}
+        <p>Privacy Policy</p>
+        <p>FAQ</p>
+      </div>
+      <div className={styles.snsLink}>
+        <a
+          href="https://www.facebook.com/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src={facebookIc} alt="페이스북 아이콘" />
+        </a>
+        <a href="https://www.x.com/ " target="_blank" rel="noreferrer noopener">
+          <img src={xIc} alt="X 아이콘" />
+        </a>
+        <a
+          href="https://www.youtube.com/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src={youtubeIc} alt="유튜브 아이콘" />
+        </a>
+        <a
+          href="https://www.instagram.com/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src={instagramIc} alt="인스타그램 아이콘" />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
